611c86526f57ab3cdb52fb77b7accd6b53ba5eac
TIm St.Clair <tim.stclair@gmail.com>
2023-01-05 15:56:02 +1100

    updated global reference

--- play.js
+++ play.js
@@ -7,7 +7,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 		typeof define === 'function' && define.amd ? define(factory) :
-			(global.Mus = factory());
+			(global.MusPlayer = factory());
 }(this, (function () {
 	'use strict';
 
